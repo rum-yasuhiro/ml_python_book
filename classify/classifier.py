942805379e12f982930cc75d6077fed2571c10e4
Yasuhiro Ohkura <rum.yasuhiro@gmail.com>
2021-09-10 11:49:10 +0900

    define super class for classifier

---
+++ classify/classifier.py
@@ -0,0 +1,15 @@
+from abc import ABCMeta, abstractclassmethod
+import numpy as np
+from numpy.random.mtrand import sample
+
+class Classifier:
+    def __init__(self, eta = 0.01, n_iter = 50, random_state = 1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def predict(self, X):
+        return np.where(self.net_input(X) >= 0.0, 1, -1)
--- classify/perceptron.py
+++ classify/perceptron.py
@@ -1,12 +1,8 @@
 import numpy as np
 from numpy.random.mtrand import sample
+from classify.classifier import Classifier
 
-class Perceptron(object):
-
-    def __init__(self, eta = 0.01, n_iter = 50, random_state = 1):
-        self.eta = eta
-        self.n_iter = n_iter
-        self.random_state = random_state
+class Perceptron(Classifier):
 
     def fit(self, X, y):
         rgen = np.random.RandomState(self.random_state)
@@ -28,10 +24,3 @@ class Perceptron(object):
 
             self.errors_.append(errors)
         return self
-    
-    def net_input(self, X):
-        return np.dot(X, self.w_[1:]) + self.w_[0]
-
-    def predict(self, X):
-        return np.where(self.net_input(X) >= 0.0, 1, -1)
-
